ff3f6164f881b52de6b529ad0f9c1259e3384f46
TurnerSF <Turner_shreve@yahoo.com>
2023-09-19 18:22:13 -0700

    started work on site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./Pages/Nav";
+import Inventory from "./Pages/Inventory"
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="container">
+        <div className="row align-items-center">
+          <div className="col-md-1">
+            <img
+              className="logo img-fluid mb-2"
+              src={logo}
+              alt="24 Hour Shifts"
+              title="24 Hour Shifts Apparel | Home"
+            />
+          </div>
+          <div className="col-md-11">
+            <div className="row">
+              <div className="col-md-6">
+                <input
+                  type="text"
+                  className="form-control border border-dark"
+                  placeholder="Search..."
+                />
+              </div>
+              <div className="col-md-6 text-center">
+                <Nav />
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
+
+      <Routes>
+        <Route path="/" element={<h1>Hello, from my router</h1>} />
+        <Route path="/shopping" element={<Inventory />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Nav.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <nav className="d-flex justify-content-end">
+      <ul className="nav">
+        <li className="nav-item">
+          <Link to="/" className="nav-link text-dark spaced-link">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/shopping" className="nav-link text-dark spaced-link">
+            Shopping
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/cart" className="nav-link text-dark spaced-link">
+            Cart
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className="nav-link text-dark spaced-link">
+            About Us
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/contact" className="nav-link text-dark">
+            Contact Us
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
